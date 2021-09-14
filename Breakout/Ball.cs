0115e50a235e336136c64c4ad29c8fcfc8234806
Korpo-spec <casper22x@gmail.com>
2021-09-14 10:32:22 +0200

    ball working

---
+++ Breakout/Ball.cs
@@ -0,0 +1,27 @@
+﻿using SFML.Graphics;
+using SFML.System;
+
+namespace Breakout
+{
+    public class Ball
+    {
+        public Sprite sprite;
+
+        public Ball()
+        {
+            sprite = new Sprite();
+            sprite.Texture = new Texture("assets/ball.png");
+            sprite.Position = new Vector2f(250, 300);
+        }
+
+        public void Update(float deltaTime)
+        {
+            
+        }
+
+        public void Draw(RenderTarget target)
+        {
+            target.Draw(sprite);
+        }
+    }
+}
--- Breakout/Program.cs
+++ Breakout/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
 
 namespace Breakout
 {
@@ -6,8 +9,27 @@ namespace Breakout
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            using (var window = new RenderWindow(new VideoMode(500, 700), "breakout"))
+            {
+                window.Closed += (o, e) => window.Close();
+                Clock clock = new Clock();
+                Ball ball = new Ball();
+                while (window.IsOpen)
+                {
+                    float deltaTime = clock.Restart().AsSeconds();
+                    window.DispatchEvents(); 
+                    //TODO: Updates
+                    ball.Update(deltaTime);
+                    window.Clear(new Color(131, 197, 235));
+                    // TODO: Drawing
+                    ball.Draw(window);
+                    window.Display();
+                    
+                }
+                
+            }
         }
     }
 }
+
+    
